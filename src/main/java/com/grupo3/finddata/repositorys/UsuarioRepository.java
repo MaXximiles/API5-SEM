87b0702893e0d51b66679c240ef20c8f8f32ef69
Maximiles <maxx.sbr@gmail.com>
2022-04-13 18:15:58 -0300

    Atualizando metodo de login

--- src/main/java/com/grupo3/finddata/repositorys/UsuarioRepository.java
+++ src/main/java/com/grupo3/finddata/repositorys/UsuarioRepository.java
@@ -8,14 +8,13 @@ import org.springframework.stereotype.Repository;
 
 import com.grupo3.finddata.classes.Empresa;
 import com.grupo3.finddata.classes.Usuario;
-import com.grupo3.finddata.classes.dto.UsuarioRs;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>  
 {
 	@Query(value = "SELECT  usu_id, usu_senha, usu_email, usu_nome, usu_nivel "
 				+ "FROM usuarios WHERE usu_email = ?1 && usu_senha = ?2 ", nativeQuery = true)
-	Usuario SelectUsuarioEmail(String UserEmail, String UserSenha);
+	List<Usuario> SelectUsuarioEmail(String UserEmail, String UserSenha);
 	
 	@Query(value = "SELECT  usu_id, usu_senha, usu_email, usu_nome, usu_nivel "
 			+ "FROM usuarios WHERE usu_nome LIKE %?1% ORDER BY usu_nome ", nativeQuery = true)
